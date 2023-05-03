2799c2931fc8c9683394e7a683b3bf1035c6e3dc
rhoitjadhav <jadhavrohit497@gmail.com>
2023-05-03 18:05:54 +0530

    files, users models

--- src/app/models.py
+++ src/app/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import text as sqlalchemy_text
 from sqlalchemy import Column, Text, ForeignKey, Integer
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy_utils import ChoiceType
 
 from app.database import db_instance
 
@@ -12,6 +13,22 @@ Base = db_instance.base
 
 def string_uuid():
     return str(uuid4())
+
+
+STATUS_TYPES = [
+    ("uploading", "Uploading"),
+    ("uploaded", "Uploaded"),
+    ("processing", "Processing"),
+    ("completed", "Completed"),
+    ("failure", "Failure")
+]
+
+FILE_TYPE = [
+    ("png", "Png"),
+    ("jpg", "Jpg"),
+    ("jpeg", "Jpeg"),
+    ("pdf", "Pdf"),
+]
 
 
 class UsersTable(Base):
@@ -36,9 +53,12 @@ class FilesTable(Base):
         server_default=sqlalchemy_text("uuid_generate_v4()"),
     )
     name = Column(Text, nullable=False)
-    path = Column(Text, nullable=False)
-    type = Column(Text, nullable=False)
-    resolution = Column(Text, nullable=False)
-    output_path = Column(Text, nullable=False)
+    path = Column(Text)
+    type = Column(ChoiceType(FILE_TYPE), nullable=False)
+    resolution = Column(Text)
+    status = Column(ChoiceType(STATUS_TYPES), nullable=False, default="uploading")
+    output_path = Column(Text)
+    output_resolution = Column(Text)
+    page_num = Column(Text)
     pdf_id = Column(UUID, ForeignKey("files.id"))
     files = relationship("FilesTable", backref=backref("parent", remote_side="FilesTable.id"))
