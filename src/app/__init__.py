d653205687fad73e42e080ab373da5e1e127dffd
rhoitjadhav <jadhavrohit497@gmail.com>
2023-05-04 18:22:17 +0530

    creating uuid-ossp extension for postgres db

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,8 @@
-from .models import UsersTable, FilesTable
+from .models import FilesTable
 
 from .database import db_instance
 
+conn = db_instance._engine.connect()
+conn.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
 db_instance.base.metadata.create_all(db_instance._engine)
 print("Database created")
