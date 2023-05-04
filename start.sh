9f9c0572bcd0ad120c64778b419cbff8e4f60671
rhoitjadhav <jadhavrohit497@gmail.com>
2023-05-04 18:22:53 +0530

    allowing pdf with ghostscript

--- start.sh
+++ start.sh
@@ -9,5 +9,8 @@ DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
 export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
+# Allowing pdf to be processed by ImageMagick-6 using ghostscript
+sed -i 's/policy domain="coder" rights="none" pattern="PDF"/policy domain="coder" rights="read|write" pattern="PDF"/g' /etc/ImageMagick-6/policy.xml
+
 # Start Gunicorn
 exec gunicorn -k "$WORKER_CLASS" -c "$GUNICORN_CONF" "$APP_MODULE"
